ba21458b5844dd6336d7baa5945f18749de929f6
Rainer <rainer@websimple.be>
2014-10-23 20:41:45 +0200

    pluginiser

---
+++ lang/paquet-joergschumann_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// J
+	'joergschumann_description' => '',
+	'joergschumann_nom' => 'Jörg Schumann',
+	'joergschumann_slogan' => '',
+);
+
+?>
